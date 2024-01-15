9e801d19b093a74a1a77e39e433becc455136876
Eduardo <eduardocasagrande71@gmail.com>
2024-01-14 23:57:46 -0300

    Fix: healthCheck log

--- src/main/java/com/wapp/core/resources/HealthCheck.java
+++ src/main/java/com/wapp/core/resources/HealthCheck.java
@@ -11,8 +11,8 @@ public class HealthCheck {
 
     @GetMapping("/healthCheck")
     public ResponseEntity<?> healthCheck() {
-        System.out.println("API is up and running!");
-        return ResponseEntity.ok("   [LOG] API is up and running!");
+        System.out.println("   [LOG] API is up and running!");
+        return ResponseEntity.ok("API is up and running!");
     }
 
 }
